fea023568bba0f63a84e8362dc8e09ea493c46b9
Venik <kbalibrea@gmail.com>
2021-08-03 00:12:18 +0200

    test unitaires 100%

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -71,50 +71,6 @@ describe("Given I am connected as an employee", ()=>{
     })
   })
 })
-// test d'intégration POST
-// describe("Given I am a user connected as Employee", () => {
-//   describe("When I create a new bill", () => {
-//     test("Add bill to mock API POST", async () => {
-//       const getSpy = jest.spyOn(firebase, "POST")
-//       const datas = {
-//         "id": "47qAXb6fIm2zOKkLzMro",
-//         "vat": "80",
-//         "fileUrl": "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
-//         "status": "pending",
-//         "type": "Hôtel et logement",
-//         "commentary": "séminaire billed",
-//         "name": "encore",
-//         "fileName": "preview-facture-free-201801-pdf-1.jpg",
-//         "date": "2004-04-04",
-//         "amount": 400,
-//         "commentAdmin": "ok",
-//         "email": "a@a",
-//         "pct": 20
-//       }
-//       const bills = await firebase.post(datas)
-//       expect(getSpy).toHaveBeenCalledTimes(1)
-//       expect(bills.data.length).toBe(5)
-//     })
-//     test("Add bill to API and fails with 404 message error", async () => {
-//       firebase.post.mockImplementationOnce(() =>
-//         Promise.reject(new Error("Erreur 404"))
-//       )
-//       const html = BillsUI({ error: "Erreur 404" })
-//       document.body.innerHTML = html
-//       const message = await screen.getByText(/Erreur 404/)
-//       expect(message).toBeTruthy()
-//     })
-//     test("Add bill to API and fails with 500 message error", async () => {
-//       firebase.get.mockImplementationOnce(() =>
-//         Promise.reject(new Error("Erreur 404"))
-//       )
-//       const html = BillsUI({ error: "Erreur 500" })
-//       document.body.innerHTML = html
-//       const message = await screen.getByText(/Erreur 500/)
-//       expect(message).toBeTruthy()
-//     })
-//   })
-// })
 
 describe("Given I am a user connected as Employee", () => {
   describe("When I create a new bill", () => {
